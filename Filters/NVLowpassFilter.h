417b7944b9b497a9cfcbbfe9ff44e57fda165a94
Bart Olsthoorn <bartolsthoorn@gmail.com>
2012-05-20 22:56:00 +0200

    added LowpassFilter

---
+++ Filters/NVLowpassFilter.h
@@ -0,0 +1,18 @@
+//
+//  NVLowpassFilter.h
+//  NVDSP
+//
+//  Created by Bart Olsthoorn on 20/05/2012.
+//  Copyright (c) 2012 Bart Olsthoorn. All rights reserved.
+//
+
+#import "NVDSP.h"
+
+@interface NVLowpassFilter : NVDSP
+
+- (void) calculateCoefficients;
+
+@property (nonatomic, assign, setter=setCornerFrequency:) float cornerFrequency;
+@property (nonatomic, assign, setter=setQ:) float Q;
+
+@end
---
+++ Filters/NVLowpassFilter.m
@@ -0,0 +1,42 @@
+//
+//  NVLowpassFilter.m
+//  NVDSP
+//
+//  Created by Bart Olsthoorn on 20/05/2012.
+//  Copyright (c) 2012 Bart Olsthoorn. All rights reserved.
+//
+
+#import "NVLowpassFilter.h"
+
+@implementation NVLowpassFilter
+
+@synthesize Q;
+@synthesize cornerFrequency;
+
+- (void) setQ:(float)_Q {
+    Q = _Q;
+    [self calculateCoefficients];
+}
+
+- (void) setCornerFrequency:(float)_cornerFrequency {
+    cornerFrequency = _cornerFrequency;
+    [self calculateCoefficients];
+}
+
+- (void) calculateCoefficients {
+    if ((cornerFrequency != 0.0f) && (Q != 0.0f)) {
+        
+        [self intermediateVariables:cornerFrequency Q:Q];
+        
+        a0 = 1 + alpha;
+        b0 = ((1 - omegaC)/2)      / a0;
+        b1 = ((1 - omegaC))        / a0;
+        b2 = ((1 - omegaC)/2)      / a0;
+        a1 = (-2 * omegaC)         / a0;
+        a2 = (1 - alpha)           / a0;
+        
+        [super setCoefficients];
+    }
+}
+
+@end
